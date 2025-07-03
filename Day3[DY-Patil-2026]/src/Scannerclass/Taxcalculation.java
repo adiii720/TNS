f8da6b0425241aaed6414cc82eb41c4b7716ae47
Aditi Ramdas Rakh <Aditi Ramdas Rakh@LAPTOP-3R9CK8KR>
2025-07-03 11:20:39 +0530

    Initial Commit

---
+++ Day3[DY-Patil-2026]/src/Scannerclass/Taxcalculation.java
@@ -0,0 +1,20 @@
+package Scannerclass;
+
+public class Taxcalculation {    
+	
+	
+       //function to calculate tax
+     public void  calculateTax(person s )
+     {
+    	 if(s.getAge ()  > 65 || s.getGender().equalsIgnoreCase("female"))
+    	 {
+    	 person.setTax(0);
+    	 System.out.println("tax is not  applicable");
+    	 
+    	 }
+    	 else if (person.getIncome() > 160000 && person.getibcome)
+    	 }
+     
+     
+     
+}
